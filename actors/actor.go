d557bab435ce0a905966ebd0042d0891fe080368
sijiaoh <sijiaoh@outlook.com>
2025-02-24 21:06:09 +0900

    Add template comment to actor

--- actors/actor.go
+++ actors/actor.go
@@ -4,11 +4,43 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+/*
+* テンプレート
+*
+* package foo
+*
+* import (
+* 	"github.com/hajimehoshi/ebiten/v2"
+* 	"github.com/sijiaoh/ebiten_shooting/actors"
+* )
+*
+* type Foo struct {
+* 	actors.ActorBase
+* }
+*
+* func NewFoo() Foo {
+* 	return Foo{
+* 		ActorBase: actors.NewActorBase(),
+* 	}
+* }
+*
+* func (f *Foo) Init() {
+* }
+*
+* func (f *Foo) Update() {
+* }
+*
+* func (f *Foo) Draw(screen *ebiten.Image) {
+* }
+*
+* func (f *Foo) OnDead() {
+* }
+ */
 type Actor interface {
+	// ライフサイクルメソッド
 	Init()
 	Update()
 	Draw(screen *ebiten.Image)
-
 	OnDead()
 
 	// 以下はActorBaseに実装済み

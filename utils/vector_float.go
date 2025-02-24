d9679cbac3ecd040438c63cd8f22a56d661a23aa
sijiaoh <sijiaoh@outlook.com>
2025-02-24 14:09:04 +0900

    Add VectorFloat methods

--- utils/vector_float.go
+++ utils/vector_float.go
@@ -1,6 +1,42 @@
 package utils
 
+import (
+	"math"
+)
+
 type VectorFloat struct {
 	x float64
 	y float64
+}
+
+func VectorFloatZero() VectorFloat {
+	return VectorFloat{0, 0}
+}
+
+func (vf *VectorFloat) Add(v VectorFloat) VectorFloat {
+	return VectorFloat{vf.x + v.x, vf.y + v.y}
+}
+
+func (vf *VectorFloat) Sub(v VectorFloat) VectorFloat {
+	return VectorFloat{vf.x - v.x, vf.y - v.y}
+}
+
+func (vf *VectorFloat) Mul(s float64) VectorFloat {
+	return VectorFloat{vf.x * s, vf.y * s}
+}
+
+func (vf *VectorFloat) Div(s float64) VectorFloat {
+	return VectorFloat{vf.x / s, vf.y / s}
+}
+
+func (vf *VectorFloat) LengthSquared() float64 {
+	return vf.x*vf.x + vf.y*vf.y
+}
+
+func (vf *VectorFloat) Length() float64 {
+	return math.Sqrt(vf.LengthSquared())
+}
+
+func (vf *VectorFloat) Normalize() VectorFloat {
+	return vf.Div(vf.Length())
 }
